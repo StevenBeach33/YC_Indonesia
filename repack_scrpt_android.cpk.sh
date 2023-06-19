711989f978a8092896122abd40179c2f99fc9b45
StevenBeach33 <107557824+StevenBeach33@users.noreply.github.com>
2023-06-19 22:15:49 +0700

    Update repack_scrpt_android.cpk.sh

--- repack_scrpt_android.cpk.sh
+++ repack_scrpt_android.cpk.sh
@@ -7,7 +7,7 @@ mkdir -p $DIR/work/Data/StreamingAssets/scrpt.cpk.contents \
 
 python3 $DIR/inucode.py
 
-wine $DIR/3rdparty/cpkmakec.exe \
+box64 wine $DIR/3rdparty/cpkmakec.exe \
         Z:$DIR/work/Data/StreamingAssets/scrpt.cpk.contents \
         Z:$DIR/out/contents/0100D12014FC2000/romfs/Data/StreamingAssets/scrpt.cpk  \
         -mode=FILENAME                              \
